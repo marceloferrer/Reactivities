f4d9722df2dc54ec9320bc7eddf2dea11e1e2cda
Marcelo Ferrer <marceloferrer@Marcelos-MacBook-Pro.local>
2020-10-04 13:16:02 -0300

    Remove unnecessary usings

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 
