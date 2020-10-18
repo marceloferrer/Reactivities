e3773062515980fabcfc0c18fa76edace15e40e3
Marcelo Ferrer <marceloferrer@Marcelos-MacBook-Pro.local>
2020-10-17 23:16:16 -0300

    Add axios agent to create, update and delete

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react'
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -21,26 +21,32 @@ const App = () => {
   }
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity])
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.create(activity).then (() => {
+      setActivities([...activities, activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+    })
   }
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity])
-    setSelectedActivity(activity);
-    setEditMode(false);
+    agent.Activities.update(activity).then (() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity])
+      setSelectedActivity(activity);
+      setEditMode(false);
+    })
   }
 
   const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(a => a.id !== id)]);
+    agent.Activities.delete(id).then (() => {
+      setActivities([...activities.filter(a => a.id !== id)]);
+    })
   }
 
   useEffect(() => {
-     axios.get<IActivity[]>('http://localhost:5000/api/activities')
+    agent.Activities.list()
     .then((response) => {
       let activities: IActivity[] = [];
-      response.data.forEach(activity => {
+      response.forEach(activity => {
         activity.date = activity.date.split('.')[0];
         activities.push(activity);
       });
