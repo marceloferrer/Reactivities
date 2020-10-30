060748869a637949419c93ddebb0fb7f0a882ce2
Marcelo Ferrer <marceloferrer@live.com.ar>
2020-10-29 23:35:37 -0300

    Change NavBar

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
 import ActivityStore from '../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { Link, NavLink } from 'react-router-dom';
 
 export const NavBar: React.FC = () => {
     const activityStore = useContext(ActivityStore);
@@ -9,13 +10,13 @@ export const NavBar: React.FC = () => {
         <div>
         <Menu fixed='top' inverted>
             <Container>
-                <Menu.Item header>
+                <Menu.Item header as={NavLink} exact to='/'>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}></img>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities'/>
+                <Menu.Item name='Activities' as={NavLink} to='/activities'/>
                 <Menu.Item>
-                    <Button onClick={activityStore.openCreateForm} positive content='Create Activity'></Button>
+                    <Button as={NavLink} to='/CreateActivity' positive content='Create Activity'></Button>
                 </Menu.Item>
             </Container>
         </Menu>
